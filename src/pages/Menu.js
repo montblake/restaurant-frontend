184a543a36f44aa6d439a511beac205a1d3b1ff1
Blake Montgomery <blakemontgomery312@gmail.com>
2021-07-08 22:17:39 -0500

    Add sub-divisions to menus

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,17 +1,144 @@
 function Menus(props) {
     console.log(props.currentMenu);
-    const currentMenuDishes = props.dishes.filter(dish => dish.menuGroup === props.currentMenu.name);
+
+    // const currentMenuDishes = props.dishes.filter(dish => dish.menuGroup === props.currentMenu.name)
     
-    //  what to do when menu data has loaded
+ 
+    
+    
+        
+   //  what to do when menu data has loaded
     const loaded = () => {
-        return currentMenuDishes.map((menuItem) => (
-            <div key={menuItem._id} className="dish">
-                <h3>{menuItem.name}</h3>
-                <h4>{menuItem.description}</h4>
-                <h5>{menuItem.price}</h5>
-            </div>
-        ));  
+        if (props.currentMenu.name === "dinner") {
+            const currentMenuDishes = [
+                props.dishes.filter(dish => dish.menuGroup === props.currentMenu.name && dish.dishType === "starter"),
+                props.dishes.filter(dish => dish.menuGroup === props.currentMenu.name && dish.dishType === "main"),
+                props.dishes.filter(dish => dish.menuGroup === props.currentMenu.name && dish.dishType === "shared")
+            ]
+    
+            const first = () => {
+                return currentMenuDishes[0].map((menuItem) => (
+                <div key={menuItem._id} className="dish">
+                    <h3>{menuItem.name}</h3>
+                    <h4>{menuItem.description}</h4>
+                    <h5>{menuItem.price}</h5>
+                </div>
+                ));  
+            }
+        
+            const second = () => {
+                return currentMenuDishes[1].map((menuItem) => (
+                <div key={menuItem._id} className="dish">
+                    <h3>{menuItem.name}</h3>
+                    <h4>{menuItem.description}</h4>
+                    <h5>{menuItem.price}</h5>
+                </div>
+                ));  
+            }
+        
+            const third = () => {
+                return currentMenuDishes[2].map((menuItem) => (
+                <div key={menuItem._id} className="dish">
+                    <h3>{menuItem.name}</h3>
+                    <h4>{menuItem.description}</h4>
+                    <h5>{menuItem.price}</h5>
+                </div>
+                ));  
+            }
+    
+            return (
+                <>
+                    <div>
+                        <h3>to start</h3>
+                        { props.dishes ? first() : "none" }
+                    </div>
+                    <div>
+                        <h3>to follow</h3>
+                        { props.dishes ? second() : "none" }
+                    </div>
+                    <div>
+                        <h3>for the table</h3>
+                        { props.dishes ? third() : "none" }
+                    </div>
+                </>
+            );
+        } else if (props.currentMenu.name === "drinks") {
+            const currentMenuDishes = [
+                props.dishes.filter(dish => dish.menuGroup === props.currentMenu.name && dish.drinkType === "cocktail"),
+                props.dishes.filter(dish => dish.menuGroup === props.currentMenu.name && dish.drinkType === "wine"),
+                props.dishes.filter(dish => dish.menuGroup === props.currentMenu.name && dish.drinkType === "zero")
+            ]
+    
+            const first = () => {
+                return currentMenuDishes[0].map((menuItem) => (
+                <div key={menuItem._id} className="dish">
+                    <h3>{menuItem.name}</h3>
+                    <h4>{menuItem.description}</h4>
+                    <h5>{menuItem.price}</h5>
+                </div>
+                ));  
+            }
+        
+            const second = () => {
+                return currentMenuDishes[1].map((menuItem) => (
+                <div key={menuItem._id} className="dish">
+                    <h3>{menuItem.name}</h3>
+                    <h4>{menuItem.description}</h4>
+                    <h5>{menuItem.price}</h5>
+                </div>
+                ));  
+            }
+        
+            const third = () => {
+                return currentMenuDishes[2].map((menuItem) => (
+                <div key={menuItem._id} className="dish">
+                    <h3>{menuItem.name}</h3>
+                    <h4>{menuItem.description}</h4>
+                    <h5>{menuItem.price}</h5>
+                </div>
+                ));  
+            }
+    
+            return (
+                <>
+                    <div>
+                        <h3>cocktails</h3>
+                        { props.dishes ? first() : "none" }
+                    </div>
+                    <div>
+                        <h3>wine</h3>
+                        { props.dishes ? second() : "none" }
+                    </div>
+                    <div>
+                        <h3>zero proof</h3>
+                        { props.dishes ? third() : "none" }
+                    </div>
+                </>
+             );
+        } else if (props.currentMenu.name === "dessert") {
+            const currentMenuDishes = props.dishes.filter(dish => dish.menuGroup === props.currentMenu.name);
+            const dessert = () => {
+                return currentMenuDishes.map((menuItem) => (
+                <div key={menuItem._id} className="dish">
+                    <h3>{menuItem.name}</h3>
+                    <h4>{menuItem.description}</h4>
+                    <h5>{menuItem.price}</h5>
+                </div>
+                ));  
+            }
+             
+            return (
+                    <div>
+                        { props.dishes ? dessert() : "none" }
+                    </div>
+             );
+        };      
     };
+
+
+
+
+        
 
     // what to do if menu data HAS NOT loaded
     const loading = () => {
@@ -33,7 +160,9 @@ function Menus(props) {
                 <div onClick={props.handleMenuRequest} id="dessertBtn">dessert</div>
             </nav>
             <div className="menu-container">
-                { props.dishes ? loaded() : loading() }
+                { props.currentMenu ? loaded() : loading()}
+                
+                
             </div>
             
         </div>
